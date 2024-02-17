2e0a3e9954484ca7ea01606b401c3e1182dda49f
chris <chrisadj92@gmail.com>
2024-02-17 14:20:22 +0000

    Added mom's name

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ name = input("What is your name?: ")
 year = int(input("What year were you born?: "))
 favorite_food = input("What is your favorite food?: ")
 favorite_sport = input("What is your favourite hobby?: ")
+mom_name = input("What is your mother's name?: ")
 
 age = 2024 - year
 
-print(f"Your name is {name} and you are {year} years old. Your favorite food is {favorite_food}. Your favorite sport is {favorite_sport}.")
+print(f"Your name is {name} and you are {year} years old. Your favorite food is {favorite_food}. Your favorite sport is {favorite_sport}. Your mother's name {mom_name}.")
 
